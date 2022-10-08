eb59645271f9da07c001dc9ca5d04775e219b6ed
joonseolee <niceweather94@gmail.com>
2022-10-09 07:14:24 +0900

    feat: 차량 이동기능 추가

--- src/main/java/racingcar/service/Car.java
+++ src/main/java/racingcar/service/Car.java
@@ -1,8 +1,12 @@
 package racingcar.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private static final String INITIAL_DISTANCE = "";
 
     private final String name;
+    private String distance;
 
     public Car(String name) {
         if (name == null || name.length() > 5 || name.length() == 0) {
@@ -10,5 +14,15 @@ public class Car {
         }
 
         this.name = name;
+        this.distance = INITIAL_DISTANCE;
+    }
+
+    public String move() {
+        int randomValue = Randoms.pickNumberInRange(0, 9);
+        if (randomValue >= 4) {
+            this.distance += "-";
+        }
+
+        return this.distance;
     }
 }
--- src/test/java/racingcar/service/CarTest.java
+++ src/test/java/racingcar/service/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,24 +9,36 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class CarTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("abcd");
+    }
+
     @Test
-    void 자동차이름_5글자초과할경우_에러() {
+    void 이름_5글자초과할경우_에러() {
         assertThatThrownBy(() -> new Car("abcdefg"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
-    void 자동차이름_0글자일경우_에러() {
+    void 이름_0글자일경우_에러() {
         assertThatThrownBy(() -> new Car(""))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
 
     @Test
-    void 자동차이름_유효할경우_객체생성() {
-        Car car = new Car("abcd");
-
+    void 이름_유효할경우_객체생성() {
         assertThat(car).isNotNull();
+    }
+
+    @Test
+    void 이동할경우_랜덤값에따라_이동하거나정차() {
+        String movedDistance = car.move();
+
+        assertThat(movedDistance).containsAnyOf("-", "");
     }
 }
